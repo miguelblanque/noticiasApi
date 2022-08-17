b521ceb0bf3d11a2463ccc1b5fbc659d126790c2
Miguel Blanque <miguelblanque@gmail.com>
2022-08-17 12:24:52 +0200

    Creacion de Formulario y Context

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Ejecucion
 cd noticiasApi
 npm install
 npm i axios
-
+npm install @mui/material @emotion/react @emotion/styled
 npm run dev
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Typography, Grid} from '@mui/material';
+import Container from '@mui/material/Container';
+import Formulario from './components/Formulario';
+import { NoticiasProvider } from './context/NoticiasProvider';
 
 
 function App() {
   
 
   return (
-   <h1>App noticias</h1>
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align='center'  marginY={5} component="h1" variant="h3">
+                Buscador de Noticias
+          </Typography>
+        </header>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+            <Grid item xs={12} md={6} lg={4}>
+              <Formulario/>
+            </Grid>
+        </Grid>
+      </Container>
+   </NoticiasProvider>
   )
 }
 
