ddd314cb8ca1710740bdf00f9161e503256795a5
Miguel Blanque <miguelblanque@gmail.com>
2022-08-18 09:48:02 +0200

    Mostrando Noticias

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import { FormControl, InputLabel, Select, MenuItem, Button, Box} from '@mui/material'
+import { FormControl, InputLabel, Select, MenuItem} from '@mui/material'
 import useNoticias from '../hooks/useNoticias'
 
 
@@ -33,15 +33,7 @@ const Formulario = () => {
                     </MenuItem>
                 ))}
             </Select>
-            <Box sx={{marginTop:2 }}>
-                <Button 
-                    fullWidth
-                    variant='contained'
-                    color='primary'
-                >
-                    Buscar Noticias
-                </Button>
-            </Box>
+            
         </FormControl>
     </form>
   )
--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import {Card , CardActions, CardContent, CardMedia, Link, Typography, Grid} from '@mui/material'
+
 
 const Noticia = ({noticia}) => {
-    console.log(noticia)
+   const { urlToImage, url, tittle, description, source} = noticia
   return (
-    <div>noticias</div>
+    <Grid item md={6} lg={4}>
+        <Card>
+          {urlToImage && (
+             <CardMedia
+             component ="img"
+             alt={`Imagen de la noticia ${tittle}`}
+             image={urlToImage}
+             height={'250'}
+         />
+          )}
+          <CardContent>
+            <Typography variant='body1' color="error">
+              {source.name}
+            </Typography>
+            <Typography variant='h5' component="div">
+              {tittle}
+            </Typography>
+            <Typography variant='body2' >
+              {description}
+            </Typography>
+          </CardContent>
+
+          <CardActions>
+            <Link 
+                href={url}
+                target="_blank"
+                variant="button"
+                width={'100%'}
+                textAlign={'center'}
+                sx={{
+                  textDecoration: 'none'
+                }}
+             >
+              Leer Noticia
+            </Link>
+          </CardActions>
+        </Card>
+    </Grid>
   )
 }
 
